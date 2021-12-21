8f6bba4958774b6762cbbbafe8976ca34afd5966
Mariana Alencastro <alencastro@evident.nl>
2021-12-21 15:14:01 +0000

    commit on master final code vue 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as userModule from '@/store/modules/userModule.js'
+import * as eventModule from '@/store/modules/eventModule.js'
+import * as notification from '@/store/modules/notification.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  modules: {
+    userModule,
+    eventModule,
+    notification,
+  },
+  state: {
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+  },
+})
